f789a1f7b8384984835f17028b3e541bca045ebb
vipul71098 <38537174+vipul71098@users.noreply.github.com>
2019-02-06 00:47:10 +0530

    Update app-routing.module.ts

--- myApp/src/app/app-routing.module.ts
+++ myApp/src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component'
-import { EventsComponent } from './events/events.component';
 import { SpecialEventsComponent } from './special-events/special-events.component';
 import { AuthGuard } from './auth.guard';
 import { PublicComponent } from './public/public.component';
